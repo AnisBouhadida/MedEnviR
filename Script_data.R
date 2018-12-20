a34ee229f465e67831670ffa337f50291a60246b
Anis Bouhadida <AB.geekanis@outlook.fr>
2018-12-20 23:18:12 +0100

    Import data

---
+++ Script_data.R
@@ -0,0 +1,4 @@
+# Importation des donnees :
+  data_dechet <- read.csv(file = "./data/dechets-declares-au-31-12-2016.csv", header = TRUE, sep = ";")
+  data_INSEE <- read.csv(file = "./data/code-postal-code-insee-2015.csv", header = TRUE, sep = ";")
+  
