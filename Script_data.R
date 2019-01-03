63cdcfd49492b0323b0559ff49ffefe836bb124a
Anis Bouhadida <AB.geekanis@outlook.fr>
2019-01-03 16:03:56 +0100

    Correct encoding problems

--- Script_data.R
+++ Script_data.R
@@ -1,68 +1,67 @@
 # initialisation des packages:
 library(tidyverse)
+library(skimr)
 
 # Importation des donnees et Transformation en tibble:
-  data_dechet <- read.csv(file = "./data/dechets-declares-au-31-12-2016.csv", 
-                          header = TRUE, sep = ";", stringsAsFactors = FALSE ) %>% 
+  data_dechet <- read_delim(file = "./data/dechets-declares-au-31-12-2016.csv", ";",
+                            locale = locale(encoding = "ISO-8859-1"),na=c("","NA"), escape_double = F, trim_ws = F) %>% 
     tbl_df()
   
-  data_INSEE <- read.csv(file = "./data/code-postal-code-insee-2015.csv", 
-                         header = TRUE, sep = ";", stringsAsFactors = FALSE ) %>% 
-    tbl_df() 
+  data_INSEE <- read_delim(file = "./data/code-postal-code-insee-2015.csv", ";", 
+                           locale = locale(encoding = "ISO-8859-1"),na=c("","NA"), escape_double = F, trim_ws = F) %>% 
+    tbl_df()
   
 # Nettoyage des donnees: 
-  data_dechet_clean <- data_dechet[complete.cases(data_dechet),]
-  data_dechet_clean <- unique(data_dechet_clean)
-
-  data_INSEE_clean <- data_INSEE[complete.cases(data_INSEE),]
-  data_INSEE_clean <- unique(data_INSEE_clean)
+  
+  data_dechet_clean <- unique(data_dechet)
+  data_INSEE_clean <- unique(data_INSEE)
 
 # Extraction des variables interessantes et creation des dimensions:
   ED_dimensionGeo <-select(data_INSEE_clean,
-                           Geo.Point,INSEE_COM,NOM_COM,
+                           `Geo Point`,INSEE_COM,NOM_COM,
                            NOM_DEPT,NOM_REG,Code_postal) %>% 
     tbl_df() %>% add_column(id_dim_geo=round(runif(nrow(data_INSEE_clean),
                                                    min=1, max=40000)))%>%
-    distinct(Geo.Point,INSEE_COM,.keep_all = TRUE)
+    distinct(`Geo Point` ,INSEE_COM,.keep_all = TRUE)
   
   ED_dimensionDechet <-select(data_dechet_clean,
-                              GROUPE.DE.DECHETS,SOUS.GROUPE.DE.DECHETS,
-                              DESCRIPTION.PHYSIQUE,CATEGORIE,FAMILLE.IN,
-                              VOLUME.EQUIVALENT.CONDITIONNE) %>% tbl_df() %>%
+                              `GROUPE DE DECHETS`,`SOUS-GROUPE DE DECHETS`,
+                              `DESCRIPTION PHYSIQUE`,CATEGORIE,`FAMILLE IN`,
+                              `VOLUME EQUIVALENT CONDITIONNE`) %>% tbl_df() %>%
     add_column(id_dim_dechet=round(runif(nrow(data_dechet_clean), 
                                          min=1, max=7000))) %>%
-    distinct(GROUPE.DE.DECHETS,SOUS.GROUPE.DE.DECHETS,
-             DESCRIPTION.PHYSIQUE,CATEGORIE,FAMILLE.IN,
-             VOLUME.EQUIVALENT.CONDITIONNE,.keep_all = TRUE)
+    distinct(`GROUPE DE DECHETS`,`SOUS-GROUPE DE DECHETS`,
+             `DESCRIPTION PHYSIQUE`,CATEGORIE,`FAMILLE IN`,
+             `VOLUME EQUIVALENT CONDITIONNE`,.keep_all = TRUE)
   
-  ED_dimensionRadioActivite <- select(data_dechet_clean,ACTIVITE...Bq.,
-                                      MAJORATION,PRINCIPAUX.RADIONUCLEIDES) %>% 
+  ED_dimensionRadioActivite <- select(data_dechet_clean,`ACTIVITE ( Bq)`,
+                                      MAJORATION,`PRINCIPAUX RADIONUCLEIDES`) %>% 
     tbl_df() %>% add_column(id_dim_radio=round(runif(nrow(data_dechet_clean),
                                                      min=1, max=7000)))%>%
-    distinct(ACTIVITE...Bq.,PRINCIPAUX.RADIONUCLEIDES,MAJORATION,.keep_all = TRUE)
+    distinct(`ACTIVITE ( Bq)`,`PRINCIPAUX RADIONUCLEIDES`,MAJORATION,.keep_all = TRUE)
   
-  ED_dimensionProducteurDechet <-select(data_dechet_clean,NOM.DU.SITE) %>% 
+  ED_dimensionProducteurDechet <-select(data_dechet_clean,`NOM DU SITE`) %>% 
     tbl_df() %>% 
     add_column(id_dim_producteur=round(runif(nrow(data_dechet_clean), 
                                              min=1, max=7000)))%>%
-    distinct(NOM.DU.SITE,.keep_all = TRUE)
+    distinct(`NOM DU SITE`,.keep_all = TRUE)
   
   # Matching des id avec la table dechet:
 
   table_fait0 <- left_join(data_dechet_clean,ED_dimensionGeo, 
-                           by= c("CODE.INSEE"="INSEE_COM"))
+                           by= c("CODE INSEE"="INSEE_COM"))
   
   table_fait1 <- left_join(table_fait0,ED_dimensionDechet, 
-                           by= c("GROUPE.DE.DECHETS","SOUS.GROUPE.DE.DECHETS",
-                                 "DESCRIPTION.PHYSIQUE","CATEGORIE","FAMILLE.IN",
-                                 "VOLUME.EQUIVALENT.CONDITIONNE"))
+                           by= c("GROUPE DE DECHETS","SOUS-GROUPE DE DECHETS",
+                                 "DESCRIPTION PHYSIQUE","CATEGORIE","FAMILLE IN",
+                                 "VOLUME EQUIVALENT CONDITIONNE"))
   
   table_fait2 <- left_join(table_fait1,ED_dimensionRadioActivite, 
-                           by= c("ACTIVITE...Bq.","PRINCIPAUX.RADIONUCLEIDES",
+                           by= c("ACTIVITE ( Bq)","PRINCIPAUX RADIONUCLEIDES",
                                  "MAJORATION"))
   
   table_fait3 <- left_join(table_fait2,ED_dimensionProducteurDechet, 
-                           by= c("NOM.DU.SITE"))
+                           by= c("NOM DU SITE"))
   
 # Creation du fait :
   ED_faitRepartitionPoluant <- select(table_fait3,
