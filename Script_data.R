71573637fe97674bace22360d7980cc072a7b973
Anis Bouhadida <AB.geekanis@outlook.fr>
2019-01-05 21:28:23 +0100

    Replace missing values in VOL.. & Add radon

--- Script_data.R
+++ Script_data.R
@@ -8,6 +8,7 @@
 
 # initialisation des packages:
 library(tidyverse)
+library(naniar)
 
 # Importation des donnees et Transformation en tibble:
   data_dechet <- read_delim(file = "./data/dechets-declares-au-31-12-2016.csv", ";",
@@ -18,8 +19,14 @@ library(tidyverse)
                            locale = locale(encoding = "ISO-8859-1"),na=c("","NA"), 
                            escape_double = F, trim_ws = F) %>% tbl_df()
   
+  data_radon <- read_delim(file = "./data/radon.csv", ";", 
+                           locale = locale(encoding = "ISO-8859-1"),na=c("","NA"), 
+                           escape_double = F, trim_ws = F) %>% tbl_df()
+  
 # Nettoyage des donnees:
   data_dechet_clean <- unique(data_dechet)
+  data_INSEE_clean <- unique(data_INSEE)
+  data_radon_clean <- unique(data_radon)
   
   # Transformation des codes INSEE: 
   for (i in 1:nrow(data_dechet_clean))
@@ -50,30 +57,34 @@ library(tidyverse)
               })
   }
 
-  data_INSEE_clean <- unique(data_INSEE)
 # Extraction des variables interessantes et creation des dimensions:
+  Radon_cas <- select(data_radon_clean, insee_com , classe_potentiel)%>% 
+    tbl_df() %>%unique()
+  
   ED_dimensionGeo <-select(data_INSEE_clean,`Geo Point`,INSEE_COM,
                            NOM_COM,NOM_DEPT,NOM_REG,Code_postal, CODE_DEPT)%>% tbl_df() %>% 
-    rowid_to_column("id_dim_geo") %>% distinct(`Geo Point`,INSEE_COM,NOM_COM,.keep_all = TRUE) %>% 
-    separate(`Geo Point`,c("lat","lng"),sep=",")
+                           rowid_to_column("id_dim_geo") %>% 
+                           distinct(`Geo Point`,INSEE_COM,NOM_COM,.keep_all = TRUE) %>% 
+                           separate(`Geo Point`,c("lat","lng"),sep=",") %>%
+                           left_join(Radon_cas,by=c("INSEE_COM"="insee_com"))
   
   ED_dimensionDechet <- select(data_dechet_clean,`GROUPE DE DECHETS`,
                               `SOUS-GROUPE DE DECHETS`,`DESCRIPTION PHYSIQUE`,
                                CATEGORIE,`FAMILLE IN`) %>% tbl_df() %>% rowid_to_column("id_dim_dechet")%>%
-    distinct(`GROUPE DE DECHETS`,`SOUS-GROUPE DE DECHETS`,
-             `DESCRIPTION PHYSIQUE`,CATEGORIE,`FAMILLE IN`,.keep_all = TRUE)
+                               distinct(`GROUPE DE DECHETS`,`SOUS-GROUPE DE DECHETS`,
+                                        `DESCRIPTION PHYSIQUE`,CATEGORIE,`FAMILLE IN`,.keep_all = TRUE)
+  
   
   ED_dimensionRadioActivite <- select(data_dechet_clean,`PRINCIPAUX RADIONUCLEIDES`) %>% 
-    tbl_df() %>% rowid_to_column("id_dim_radio")%>%
-    distinct(`PRINCIPAUX RADIONUCLEIDES`,.keep_all = TRUE)
+                                      tbl_df() %>% rowid_to_column("id_dim_radio")%>%
+                                      distinct(`PRINCIPAUX RADIONUCLEIDES`,.keep_all = TRUE)
   
   ED_dimensionProducteurDechet <-select(data_dechet_clean,`NOM DU SITE`) %>% 
-    tbl_df() %>% 
-    rowid_to_column("id_dim_producteur")%>%
-    distinct(`NOM DU SITE`,.keep_all = TRUE)
+                                        tbl_df() %>% 
+                                        rowid_to_column("id_dim_producteur")%>%
+                                        distinct(`NOM DU SITE`,.keep_all = TRUE)
   
   # Matching des id avec la table dechet:
-
   table_fait0 <- left_join(data_dechet_clean,ED_dimensionGeo, 
                            by=c("CODE INSEE"="INSEE_COM"))
   
@@ -92,15 +103,15 @@ library(tidyverse)
                                       id_dim_geo,id_dim_dechet,
                                       id_dim_radio,id_dim_producteur,
                                       `VOLUME EQUIVALENT CONDITIONNE`,
-                                      `ACTIVITE ( Bq)`,MAJORATION) %>% drop_na(id_dim_geo)
-  ED_faitRepartitionPoluant <-rowid_to_column(ED_faitRepartitionPoluant,"id_fait")
+                                      `ACTIVITE ( Bq)`) %>% 
+                               drop_na(id_dim_geo) %>%
+                               rowid_to_column("id_fait") %>% 
+                               replace_with_na(replace = list(`VOLUME EQUIVALENT CONDITIONNE`="-",
+                                                              `ACTIVITE ( Bq)`="-"))
   
-  
-
 # Exportation de l'entrepot de donnees : 
-  if (!file.exists("EntrepotDeDonnees")){
-    dir.create("EntrepotDeDonnees")
-    }
+  if (!file.exists("EntrepotDeDonnees")){dir.create("EntrepotDeDonnees")}
+  
   write.csv(ED_dimensionDechet,file = "./EntrepotDeDonnees/ED_dimensionDechet.csv", 
             row.names = FALSE)
   write.csv(ED_dimensionGeo,file = "./EntrepotDeDonnees/ED_dimensionGeo.csv", 
@@ -113,5 +124,5 @@ library(tidyverse)
             row.names = FALSE)
 
 # Nettoyage de l'environnement et des objets temporaires:
-rm(data_dechet,data_INSEE)
-rm(table_fait0,table_fait1,table_fait2,table_fait3,i)
+rm(data_dechet,data_INSEE,data_radon)
+rm(Radon_cas,table_fait0,table_fait1,table_fait2,table_fait3,i,num_arr)
